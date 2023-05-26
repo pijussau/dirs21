1dfe5df4cfbe75fbf9cb5e8c30e28a0c8eb6d517
Your Name <you@example.com>
2023-05-26 19:52:35 +0300

    Created dashboard

---
+++ app/src/pages/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div class="col-xl-3 col-lg-5 col-md-6">
+      <user-card> </user-card>
+    </div>
+
+    <div class="col-xl-3 col-lg-5 col-md-6">
+      <user-card> </user-card>
+    </div>
+
+    <div class="col-xl-3 col-lg-5 col-md-6">
+      <user-card> </user-card>
+    </div>
+
+    <div class="col-xl-3 col-lg-5 col-md-6">
+      <user-card> </user-card>
+    </div>
+    
+  </div>
+</template>
+<script>
+import EditProfileForm from "./UserProfile/EditProfileForm.vue";
+import UserCard from "./UserProfile/UserCard.vue";
+import MembersCard from "./UserProfile/MembersCard.vue";
+export default {
+  components: {
+    EditProfileForm,
+    UserCard,
+    MembersCard,
+  },
+};
+</script>
+<style></style>
---
+++ app/src/router/routes.js
@@ -0,0 +1,30 @@
+import DashboardLayout from "@/layout/dashboard/DashboardLayout.vue";
+import NotFound from "@/pages/NotFoundPage.vue";
+import Dashboard from "@/pages/Dashboard.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "dashboard",
+        component: Dashboard,
+      }
+    ],
+  },
+  { path: "*", component: NotFound },
+];
+
+/**
+ * Asynchronously load view (Webpack Lazy loading compatible)
+ * The specified component must be inside the Views folder
+ * @param  {string} name  the filename (basename) of the view to load.
+function view(name) {
+   var res= require('../components/Dashboard/Views/' + name + '.vue');
+   return res;
+};**/
+
+export default routes;
